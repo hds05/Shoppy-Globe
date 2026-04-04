d8808a6aed2d77fa35d24960575aa3fbd5c0b18d
hds05 <duttsharmahimanshu96@gmail.com>
2026-04-04 23:57:36 +0530

    Integrated React Router and added 404 error handling. Improved NotFoundPage with error display and navigation.

--- src/Components/NotFoundPage.jsx
+++ src/Components/NotFoundPage.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Link, useRouteError } from 'react-router-dom'
 
 function NotFoundPage() {
+  const err = useRouteError();
+  console.log(err);
+
   return (
-    <div>NotFoundPage</div>
+    <div className='flex relative justify-center items-center h-screen p-6 '>
+      <div className='errMsg  shadow-[0px_5px_10px_gray] z-20 p-4 rounded-2xl text-center bg-white'>
+        <img src="/404.gif" className='w-100' alt="" />
+        <h1 className='text-2xl'>
+          {err.status}: {err.statusText}
+        </h1>
+        {err.error.message}
+        <div className='p-2 rounded-2xl bg-[rgba(9_0_31)] text-white cursor-pointer mt-2'>
+          <Link to={'/'}>Back to Home</Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import NotFoundPage from './Components/NotFoundPage.jsx'
+import ProductList from './Components/ProductList.jsx'
 
+
+const appRouter = createBrowserRouter([
+  {
+    path:'/',
+    element: <App />,
+    errorElement: <NotFoundPage />
+  },
+  {
+    path:'/productlist',
+    element: <ProductList />
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={appRouter} />
   </StrictMode>,
 )
