075ee64ba8b34d3b5bb679df89da2337507ea626
hds05 <duttsharmahimanshu96@gmail.com>
2026-04-04 17:37:36 +0530

    Created Header component and integrated in App component. Integrated tailwindcss in project. Added shopping cart icon.

---
+++ src/Components/Header.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+
+function Header(){
+    const [cartData, setCartData] = useState(0)
+
+    return (
+        <>
+            <header className="flex justify-between px-10 py-6 shadow-[1px_0px_2px_gray]">
+                <div className="text-4xl">ShoppyGlobe</div>
+                {/* <nav className="text-xl flex gap-2">
+                    <a href="">About</a>
+                    <a href="">Categories</a>
+                </nav> */}
+                <div className="relative">
+                    <img src="/shopping-cart.png" width={'40px'} alt="" />
+                    <span className="absolute bottom-4 left-8 bg-white rounded-full shadow-[0px_1px_5px_gray] p-1 text-[10px]">{cartData}</span>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header;
