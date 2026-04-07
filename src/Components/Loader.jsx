b91ec54378783641376a6e35106a61ed568670ea
hds05 <duttsharmahimanshu96@gmail.com>
2026-04-07 11:19:25 +0530

    Created Loader component and integrate it into ProductList for improved loading experience

---
+++ src/Components/Loader.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Hourglass } from 'react-loader-spinner'
+
+function Loader() {
+    return (
+        <div className='w-full h-screen flex justify-center items-center'>
+            (<Hourglass
+                visible={true}
+                height="80"
+                width="80"
+                ariaLabel="hourglass-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+                colors={['black', 'green']}
+            />)
+        </div>
+    )
+}
+
+export default Loader
--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import { useCustomHook } from '../utils/useCustomHook';
 import ProductItem from './ProductItem';
+import Loader from './Loader';
 
 function ProductList() {
   const { productData, loading } = useCustomHook();
   console.log([productData, loading, 'data from custom hook']);
   // const [productData, setProductData] = useState(data)
-  if (loading) return <h1>Loading...</h1>
+  // can i use useEffect here to use setTimeOut for loader???
+  if (loading) return <Loader />
   return (
     <>
       <div className='grid grid-cols-4 p-4 justify-center gap-6'>
