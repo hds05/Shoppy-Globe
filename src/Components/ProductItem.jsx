389566478d9ea5ee32f447ed6cd75291e8ec63b2
hds05 <duttsharmahimanshu96@gmail.com>
2026-04-08 10:51:06 +0530

    updated ProductItem component with basic styling and added add to cart button in the card.

--- src/Components/ProductItem.jsx
+++ src/Components/ProductItem.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react'
 
-function ProductItem(props) { 
+function ProductItem(props) {
   return (
     <>
-      <div id={props.data.id} className='flex flex-col justify-between shadow-[0px_0px_3px_black] p-2 px-4 rounded-2xl'>
+      <div id={props.data.id} className='flex flex-col justify-between shadow-[0px_0px_3px_black] p-2 px-4 md:rounded-2xl'>
         <h1 className='text-2xl font-semibold'>{props.data.title}</h1>
-        <img src={props.data.thumbnail} width={'150px'} alt="" />
+        <div className='flex justify-center my-4'>
+          <img src={props.data.thumbnail} width={'150px'} alt="" />
+        </div>
         <h1>{props.data.category}</h1>
-        <p>{props.data.description}</p>
+        <p className='text-sm md:text-md text-gray-500'>{props.data.description}</p>
+        <div className='text-center my-4'>
+          <button className='bg-black text-white rounded-4xl p-0 text-[12px] md:text-md px-2 md:px-4 py-2'>Add to cart</button>
+        </div>
       </div>
     </>
   )
