b9c28ea5f6b3f5c99a1febe3cbcf4dc8612e1970
hds05 <duttsharmahimanshu96@gmail.com>
2026-04-17 20:46:18 +0530

    Updated ProductDetail with a basic product information layout and updated ProductItem to link dynamically to the ProductDetail page using dynamic routing.

--- src/Components/ProductDetail.jsx
+++ src/Components/ProductDetail.jsx
@@ -1,8 +1,83 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useCustomHook } from '../utils/useCustomHook';
+import Loader from './Loader';
 
 function ProductDetail() {
+  const [productDetail, setProductDetail] = useState([])
+  const { data, loading, error } = useCustomHook()
+  // console.log(data);
+
+  const { id } = useParams()
+  console.log(id, "id from detail");
+  useEffect(() => {
+    if (data) {
+      const detailOfProduct = data.find((e) => e.id == id)
+      console.log(detailOfProduct, "detail of product form detail page");
+      setProductDetail(detailOfProduct)
+    }
+  }, [data])
+  // console.log(productDetail);
+  if (loading) return <Loader />
+  if (error) return <div className='flex justify-center items-center m-2'>
+    <div className='flex flex-col items-center w-fit p-4 shadow-[0px_0px_2px_gray] rounded-4xl'>
+      <img src="/404.gif" width={'250px'} alt="" />
+      <p>Something went wrong while fetching the data...{error}</p>
+    </div>
+  </div>
+
   return (
-    <div>ProductDetail</div>
+    <div className="m-6">
+      <div className="bg-white rounded-3xl p-6 flex flex-col lg:flex-row gap-8">
+        <div className="w-1/2 flex flex-col items-center">
+          <img
+            src={productDetail?.thumbnail}
+            alt={productDetail?.title}
+            className="w-[350px] rounded-2xl object-cover"
+          />
+        </div>
+        <div className='w-1/2 flex flex-col gap-3 p-4'>
+          <h1 className='text-3xl font-bold text-gray-800'>
+            {productDetail?.title}
+          </h1>
+          <p className='text-gray-600'>
+            Brand: <span className='font-medium'>{productDetail?.brand}</span>
+          </p>
+          <p className='text-gray-600'>
+            Category: <span className='capitalize'>{productDetail?.category}</span>
+          </p>
+          <p className='text-gray-600'>
+            Rating: {productDetail?.rating}
+          </p>
+          <div className='flex items-center gap-4'>
+            <p className='text-2xl font-bold'>
+              ${productDetail?.price}
+            </p>
+            <p >
+              {productDetail?.discountPercentage}% OFF
+            </p>
+          </div>
+          <p className='text-gray-700'>
+            {productDetail?.description}
+          </p>
+          <p className='text-gray-600'>
+            Stock: <span className='font-medium'>{productDetail?.stock}</span>
+          </p>
+          <p>
+            {productDetail?.availabilityStatus}
+          </p>
+
+          <div className="bg-gray-200 rounded-2xl p-4 flex flex-col gap-2">
+            <p>{productDetail?.shippingInformation}</p>
+            <p> {productDetail?.returnPolicy}</p>
+            <p> {productDetail?.warrantyInformation}</p>
+          </div>
+
+        </div>
+      </div>
+
+
+    </div>
   )
 }
 
--- src/Components/ProductItem.jsx
+++ src/Components/ProductItem.jsx
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function ProductItem(props) {
+  console.log(props, ' This is props room product item');
+
   return (
     <>
-      <div id={props.data.id} className='flex flex-col justify-between shadow-[0px_0px_3px_black] p-2 px-4 md:rounded-2xl'>
-        <h1 className='text-2xl font-semibold'>{props.data.title}</h1>
-        <div className='flex justify-center my-4'>
-          <img src={props.data.thumbnail} width={'150px'} alt="" />
+      <Link to={`/product/${props.data.id}`}>
+        <div id={props.data.id} className='flex flex-col h-[450px] justify-between shadow-[0px_0px_3px_black] p-2 px-4 md:rounded-2xl'>
+          <h1 className='text-2xl font-semibold'>{props.data.title}</h1>
+          <div className='flex justify-center my-4'>
+            <img src={props.data.thumbnail} width={'150px'} alt="" />
+          </div>
+          <h1>{props.data.category}</h1>
+          <p className='text-sm md:text-md text-gray-500'>{props.data.description}</p>
+          <div className='text-center my-4'>
+
+            <button className='bg-black text-white rounded-4xl p-0 text-[12px] md:text-md px-2 md:px-4 py-2'>Add to cart</button>
+          </div>
         </div>
-        <h1>{props.data.category}</h1>
-        <p className='text-sm md:text-md text-gray-500'>{props.data.description}</p>
-        <div className='text-center my-4'>
-          <button className='bg-black text-white rounded-4xl p-0 text-[12px] md:text-md px-2 md:px-4 py-2'>Add to cart</button>
-        </div>
-      </div>
+      </Link>
     </>
   )
 }
