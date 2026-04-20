cfe452d070e5a1cabad606936a530cf6479cf485
hds05 <duttsharmahimanshu96@gmail.com>
2026-04-20 12:08:11 +0530

    Add cancel icon and implemented functionality to clear the search input in Search component.

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,19 +1,33 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { selectSearchText, setSearchText } from "../utils/searchSlice";
+import { selectSearchText, setInputEmpty, setSearchText } from "../utils/searchSlice";
 
 function Search(props) {
 
+    // getting searched text from redux store
     const search = useSelector(selectSearchText)
+
+    // dispatch hook to send actions to Redux store
     const dispatch = useDispatch()
 
+    // function to dispatch the searched text to Redux store whenever the user types
     function handleSearch(e) {
         dispatch(setSearchText(e.target.value))
     }
 
     return (
+        // container for search input
         <div className="flex m-8 gap-4 justify-center">
-            <input type="text" placeholder="Search here..." className="bg-white rounded-2xl w-lg shadow-[0px_2px_6px_gray] p-2" value={search} onChange={handleSearch} />
+            <input type="text" placeholder="Search here..." className="bg-white rounded-2xl w-lg shadow-[0px_2px_6px_gray] p-2"
+                // input value comes from redux store
+                value={search}
+                // updates the Reduux state whenever input changes
+                onChange={handleSearch} />
+            {
+                search.length ? (<button onClick={()=> dispatch(setInputEmpty())} className="cursor-pointer">
+                    <img src="/cancel.png" className="w-[35px]" alt="" />
+                </button>) : ''
+            }
 
         </div>
     )
